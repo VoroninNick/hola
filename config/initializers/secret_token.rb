1b188b71bed3c75c5d164ef7d93d9f5b742e3229
pasha <p.korenev@voroninstudio.eu>
2014-02-10 18:37:03 +0200

    version 0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hol::Application.config.secret_key_base = '0193cf91c3b0780a37ae770ac4483cde5168be23c5e22f7b96a302579c49b8618af2ade286962976f40c10dde042b5ebee51140a63ecf7904473b5343c0f97ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hol::Application.config.session_store :cookie_store, key: '_hol_session'
