e7578b850998567b872c7540ed195827c90d1bb2
pasha <p.korenev@voroninstudio.eu>
2014-02-11 14:31:44 +0200

    tuesday-afternoon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Hol::Application.routes.draw do
 
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root to: "page#index"
   # The priority is based upon order of creation: first created -> highest priority.
